f335c05d538576c5f91d21c603d4e4e6754c9b5b
Dani <dserrano.314@protonmail.com>
2021-04-22 19:35:33 +0200

    Primer commit

---
+++ Cambios/cambios.java
@@ -0,0 +1,6 @@
+public class cambios {
+    public static void main(String[] args) {
+        System.out.println("Aqui sufriré los cambios.");
+        System.out.println(0);
+    }
+}
---
+++ src/Actividad_41.java
@@ -0,0 +1,5 @@
+public class Actividad_41 {
+    public static void main(String[] args) {
+        System.out.println("Hola, Juan!");
+    }
+}
