fd3b6c8764d616a88179e4d52f0fa8f7738d9e76
Dani <dserrano.314@protonmail.com>
2021-04-22 20:01:58 +0200

    Cambio 2.

--- Cambios/cambios.java
+++ Cambios/cambios.java
@@ -1,6 +1,6 @@
 public class cambios {
     public static void main(String[] args) {
         System.out.println("Aqui cuento los cambios.");
-        System.out.println(1);
+        System.out.println(2);
     }
 }
